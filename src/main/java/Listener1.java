bf20de910dfd5df4ae5f8dd37af68a0b07f91b77
StossenHDYT <47639297+StossenHDYT@users.noreply.github.com>
2021-03-21 18:22:28 +0100

    Update v 1.1

--- src/main/java/Listener1.java
+++ src/main/java/Listener1.java
@@ -5,26 +5,44 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.ArrayList;
+
 public class Listener1 implements Listener {
     public static int clicks = 0;
     public static int schedule;
 
     @EventHandler
     public void onClick(PlayerInteractEvent e){
+        Player t = e.getPlayer();
 
-        int ping = (((CraftPlayer)Bukkit.getPlayer("SatophdieLegende ")).getHandle()).ping;
+        int ping = (((CraftPlayer)Bukkit.getPlayer(e.getPlayer().getName())).getHandle()).ping;
         clicks++;
-        schedule = Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
-            @Override
-            public void run() {
-                for (Player all : Bukkit.getOnlinePlayers()){
-                    all.getPlayer().sendMessage("§7[§3CPS§7] Klicks von §aStophdieLegende §8» §b" + clicks + " §7︳ " + "§a" + ping + "ms");
-                }
 
-                clicks = 0;
-                Bukkit.getScheduler().cancelAllTasks();
-            }
-        },20);
+            schedule = Bukkit.getScheduler().scheduleSyncDelayedTask(Main.plugin, new Runnable() {
+                @Override
+                public void run() {
+
+                    for (String spielers : Main.penis) {
+
+                        Player p = Bukkit.getPlayer(spielers);
+
+                        
+
+                        p.getPlayer().sendMessage("§7[§3CPS§7] Klicks von §a" + e.getPlayer().getName() + " §8» §b" + clicks + " §7︳ " + "§a" + ping + "ms");
+
+                    }
+
+
+
+                    clicks = 0;
+                    Bukkit.getScheduler().cancelAllTasks();
+                }
+            },20);
+
+
+
+
+
 
 
     }
